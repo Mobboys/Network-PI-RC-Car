1272fb9e68df120b927a29bdf9f5bef6cdb255fa
Mobboys <Vance@praxus.com>
2023-12-08 17:41:15 -0800

    new new trial

--- Client.py
+++ Client.py
@@ -6,6 +6,7 @@ import threading
 import cv2
 import pickle
 import sys
+import struct
 
 
 # C:\Users\vance\PycharmProjects\RC_Client\venv\Scripts\python.exe
@@ -174,6 +175,34 @@ def new_main():
                 break
                 
             #print(full_msg)
+
+def new_new_main():
+    # create socket
+    client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    host_ip = '192.168.1.20' # paste your server ip address here
+    port = 9999
+    client_socket.connect((host_ip,port)) # a tuple
+    data = b""
+    payload_size = struct.calcsize("Q")
+    while True:
+        while len(data) < payload_size:
+            packet = client_socket.recv(4*1024) # 4K
+            if not packet: break
+            data+=packet
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("Q",packed_msg_size)[0]
+        
+        while len(data) < msg_size:
+            data += client_socket.recv(4*1024)
+        frame_data = data[:msg_size]
+        data  = data[msg_size:]
+        frame = pickle.loads(frame_data)
+        cv2.imshow("RECEIVING VIDEO",frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key  == ord('q'):
+            break
+    client_socket.close()
 
 if __name__ == '__main__':
     new_main()
--- PIServer.py
+++ PIServer.py
@@ -3,16 +3,17 @@ import RPi.GPIO as GPIO
 import time
 import cv2
 import pickle
+import struct
 
 
 def receive(s, bufferSize):
     data = s.recv(bufferSize)              #Latest Error (Matching Client.py) - OSError: [Errno 107] Transport endpoint is not connected
     data = data.decode('utf-8')
     controllerInputs = [float(x) for x in data.split(',')]
-    return controllerInputs, address
+    return controllerInputs
 
 
-def send(address, s, cap):
+def send(s, cap):
     _, frame = cap.read()
     serialized = pickle.dumps("test")
     print (serialized)
@@ -62,9 +63,9 @@ def main():
             print('Connected by', addr)
             
             while True:
-                controllerInputs, address = receive(s, bufferSize)
+                controllerInputs = receive(s, bufferSize)
                 lastAngle = motorControl(controllerInputs, lastAngle, servo1)
-                send(address, s, cap)  # uh oh he too big
+                send(s, cap)  # uh oh he too big
 
 def new_main():
     FRAME_WIDTH = 1920 // 10000
@@ -92,7 +93,35 @@ def new_main():
 
         time.sleep(4)
 
-    
+def new_new_main():
+        # Socket Create
+    server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    host_name  = socket.gethostname()
+    host_ip = socket.gethostbyname(host_name)
+    print('HOST IP:',host_ip)
+    port = 9999
+    socket_address = (host_ip,port)
+
+    # Socket Bind
+    server_socket.bind(socket_address)
+
+    # Socket Listen
+    server_socket.listen(5)
+    print("LISTENING AT:",socket_address)
+
+    # Socket Accept
+    while True:
+        client_socket,addr = server_socket.accept()
+        print('GOT CONNECTION FROM:',addr)
+        if client_socket:
+            vid = cv2.VideoCapture(0)
+            
+            while(vid.isOpened()):
+                img,frame = vid.read()
+                a = pickle.dumps(frame)
+                message = struct.pack("Q",len(a))+a
+                client_socket.sendall(message)
+                
 
 
 if __name__ == "__main__":
