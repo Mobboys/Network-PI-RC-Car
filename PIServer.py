e0b387b96384d59b26617940d927cb80422456b6
Mobboys <95667289+Mobboys@users.noreply.github.com>
2024-02-25 17:55:20 -0800

    Update PIServer.py

--- PIServer.py
+++ PIServer.py
@@ -144,7 +144,6 @@ def main():
             while True:
                 controllerInputs = receive(conn, bufferSize)
                 lastAngle = motorControl(controllerInputs, lastAngle, pwm, Encoded)
-                time.sleep(.01)
                 # send(s, cap)  # uh oh he too big
 
 def new_main():
